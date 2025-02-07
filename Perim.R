9b164dbf32a9ecc8736398e802c06efbb1d39958
marcdome <marcdome@alumni.uv.es>
2025-02-07 23:25:23 +0100

    2*base+2*altura

--- Perim.R
+++ Perim.R
@@ -6,6 +6,6 @@ LongitudCircunferencia<-function(R){
   return(2*pi*R)
 }
 
->PerimetroRectangulo <- function(base, altura) {
-  return(2 * (base + altura))
+PerimetroRectangulo <- function(base, altura) {
+  return(2*base+2*altura)
 }
